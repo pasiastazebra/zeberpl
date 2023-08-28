72430dc82e7c2cfcd58e1934c31d02fe6763d490
pasiastazebra <vobis08gw@gmail.com>
2023-08-29 00:10:52 +0200

    Update project.vue

--- zeberpl/src/components/project.vue
+++ zeberpl/src/components/project.vue
@@ -10,12 +10,13 @@ export default {
     <div class="project-container">
         <h3 class="project-title">test</h3>
         <img src="../assets/projects/login.png" alt="" class="project-image">
+        <p class="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatum atque dolorem sapiente ut, unde repellat eius adipisci! Molestiae voluptatibus tenetur repudiandae molestias officiis eum eligendi, sapiente error quis ducimus!</p>
         <div class="project-buttons">
             <div class="project-buttons-cover">
-                <button id="source">Source</button>
+                <button id="source" class="button">Source</button>
             </div>
             <div class="project-buttons-cover">
-                <button id="demo">Demo</button>
+                <button id="demo" class="button">Demo</button>
             </div>
         </div>
     </div>
@@ -44,7 +45,14 @@ export default {
     &-image {
         max-width: 90%;
         margin: 2% 5%;
+
+        border: none;
+        border-radius: 20px;
         
+    }
+
+    &-description {
+        margin: 2% 5%;
     }
 
     &-buttons {
@@ -56,43 +64,37 @@ export default {
             justify-content: center;
         }
 
-        #source {
-            min-width: 50%;
+        button {
             color: #fff;
-            background: linear-gradient(61deg, rgba(1, 116, 1, 0.6) 0%, rgba(0, 112, 65, 0.6) 100%);
             padding: 10px 30px;
-
             border: none;
             border-radius: 50px;
-
             box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;
             transition : 332ms;
             cursor: pointer;
+            min-width: 50%;
+            margin: 5%;
+        }
+
+        #source {
+            background: linear-gradient(61deg, rgba(1, 116, 1, 0.6) 0%, rgba(0, 112, 65, 0.6) 100%);
 
             &:hover{
             transition : 332ms;
-            padding: 10px 50px;
+
+            background: linear-gradient(61deg, rgba(1, 116, 1, 0.9) 0%, rgba(0, 112, 65, 0.9) 100%);
             min-width: 70%;
             }
         }
 
         #demo {
-            min-width: 50%;
-
-            color: #fff;
             background: linear-gradient(61deg, rgba(26, 1, 116, 0.6) 0%, rgba(1, 66, 126, 0.6) 100%);
-            padding: 10px 30px;
 
-            border: none;
-            border-radius: 50px;
-
-            box-shadow: rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset;
-            transition : 332ms;
-            cursor: pointer;
 
             &:hover{
             transition : 332ms;
-            //padding: 10px 50px;
+
+            background: linear-gradient(61deg, rgba(26, 1, 116, 0.9) 0%, rgba(1, 66, 126, 0.9) 100%);
             min-width: 70%;
             }
         }
