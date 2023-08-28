d84f8aa6f5046d2421aa88101805df048b9a5b32
pasiastazebra <vobis08gw@gmail.com>
2023-08-28 18:44:27 +0200

    transition and color fixed

--- zeberpl/src/App.vue
+++ zeberpl/src/App.vue
@@ -96,7 +96,7 @@ nav {
     font-size: 20px;
     text-decoration: none;
     color: var.$text;
-    transition: 0.3s;
+    transition: 0.2s;
 
     &.router-link-exact-active {
       color: darken($color: var.$text, $amount: 20%);
