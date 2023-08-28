7cd9db4776406e7cd6b653c400c000f6da905065
pasiastazebra <vobis08gw@gmail.com>
2023-08-28 20:58:46 +0200

    working icon component

---
+++ zeberpl/src/components/stack.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+    props: ['icons']
+}
+
+</script>
+
+<template>
+    <div class="svg-container">
+      <img v-for="icon in icons" :src="icon">
+    </div>
+  </template>
+
+<style scoped lang="scss">
+ @use "../assets/SectionStyle";
+</style>
--- zeberpl/src/components/views/AboutView.vue
+++ zeberpl/src/components/views/AboutView.vue
@@ -1,17 +1,30 @@
 <script setup>
 
+import stack from '../stack.vue';
+
 const aboutMe = `I am an aspiring Front-End developer for whom it is a priority to constantly expand my horizons.
                  I want to create websites that are aesthetically pleasing, functional and above all efficient.
                  Outside of hours I am interested in aviation, sociology, planning and geography in general.
                  In moments of inspiration I also play with vector graphics. Bad coffee is always better than
                  no coffee at all.`;
 
+const iKnowIcons = [
+    'src/assets/stack/iknow/bootstrap.svg',
+    'src/assets/stack/iknow/sass.svg',
+    'src/assets/stack/iknow/ts.svg',
+    'src/assets/stack/iknow/vite.svg',
+    'src/assets/stack/iknow/vue.svg',
+];
+
 </script>
 
 <template>
     <div class="article-wrapper">
-        <h1>About me</h1>
+        <h1>Home</h1>
+        <h2>About me</h2>
         <p>{{ aboutMe }}</p>
+        <h2>Tech stack</h2>
+        <stack :icons="iKnowIcons"/>  
     </div>
 </template>
 
