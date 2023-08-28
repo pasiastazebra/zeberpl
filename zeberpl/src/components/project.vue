ff641730519ad042fe3ce2b09e70d5843d759689
pasiastazebra <vobis08gw@gmail.com>
2023-08-28 23:31:32 +0200

    project component added

---
+++ zeberpl/src/components/project.vue
@@ -0,0 +1,48 @@
+<script>
+
+export default {
+    props: ['project']
+}
+
+</script>
+
+<template>
+    <div class="project-container">
+        <h3 class="project-title">test</h3>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../assets/variables" as var;
+
+.stack{
+
+    &-container {
+        background: var.$secondary;
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.342);
+        backdrop-filter: blur(8.3px);
+        -webkit-backdrop-filter: blur(8.3px);
+
+        margin: 5px;
+        min-width: 140px;
+    }
+    &-title {
+        text-align: center;
+    }
+}
+.svg{
+    &-container {
+       display: grid;
+       grid-template-columns: 70px 70px;
+    }
+    &-cover {
+        display: flex;
+        justify-content: center;
+    }
+}
+.icon {
+    max-width: 60px;
+    margin: 5px;
+}
+</style>
--- zeberpl/src/components/views/ProjectsView.vue
+++ zeberpl/src/components/views/ProjectsView.vue
@@ -1,12 +1,14 @@
 <script setup>
+import project from '../project.vue';
+
 
 </script>
 
 <template>
     <div class="article-wrapper">
         <h1>Projects</h1>
-        <p>Proin a massa vel felis dictum elementum vitae eu tellus. Integer volutpat, lacus eget tristique semper, arcu orci feugiat purus, at fringilla magna nisi consequat felis. Nunc ut tincidunt nibh. Vivamus nec nisi in leo lacinia venenatis. Aenean vitae suscipit nunc. Integer gravida felis turpis, quis consequat elit lobortis vel. Vestibulum ut urna eros. Quisque mattis tempor felis, et consectetur urna malesuada et. Nunc congue, metus non pellentesque rutrum, risus massa facilisis ante, vitae accumsan lacus leo sodales nulla. Proin lorem nibh, interdum vitae bibendum nec, tincidunt ut elit. Nunc ut enim tempus metus finibus porta non a orci. Fusce elit sem, pulvinar scelerisque tempor ut, porttitor quis ex. Mauris a enim quis arcu blandit suscipit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
-    </div>
+        <project />
+   </div>
 </template>
 
 <style scoped lang="scss">
